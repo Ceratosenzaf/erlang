a6932b180dce4c05e6683de5e3d4fb347fe71c23
Ceratosenzaf <ceratosenzaf@gmail.com>
2024-11-16 11:48:28 +0100

    fix: lab 2 - ex 5

--- lab2_5.erl
+++ lab2_5.erl
@@ -1,5 +1,5 @@
 -module(lab2_5).
--export([set_cookie/0, connect/1, connect/2, disconnect/0, send/1]).
+-export([set_cookie/0, connect/1, connect/2, disconnect/0, send/1, peers/0]).
 
 set_cookie() -> erlang:set_cookie(node(), 'cookie').
 
@@ -8,17 +8,17 @@ connect(User, Peer) ->
     set_cookie(),
     Pid = spawn(
         fun() ->
-            put(user, User),
             case Peer of
                 undefined -> ok;
                 Pid -> Pid ! {login, {User, self()}}
             end,
+            process_flag(trap_exit, true),
+            put(user, User),
             loop([])
         end
     ),
     put(user, User),
     register(client, Pid),
-    put(client, Pid),
     ok.
 
 disconnect() ->
@@ -29,11 +29,16 @@ send(Msg) ->
     client ! {msg, Msg},
     ok.
 
+peers() ->
+    client ! {peers},
+    ok.
+
 loop(Peers) ->
     receive
         {login, {User, Pid}} ->
             io:format("[client]: ~p logged in~n", [User]),
             Pid ! {login_ack, Peers, {get(user), self()}},
+            link(Pid),
             lists:foreach(fun({_, P}) -> P ! {p2p, login, {User, Pid}} end, Peers),
             loop([{User, Pid} | Peers]);
         {login_ack, P1, Peer} ->
@@ -46,8 +51,12 @@ loop(Peers) ->
             io:format("[client] You: ~p~n", [Msg]),
             lists:foreach(fun({_, P}) -> P ! {p2p, msg, Msg, {get(user), self()}} end, Peers),
             loop(Peers);
+        {peers} ->
+            io:format("[client] Peers: ~p~n", [Peers]),
+            loop(Peers);
         {p2p, login, {User, Pid}} ->
             io:format("[client]: ~p logged in~n", [User]),
+            link(Pid),
             loop([{User, Pid} | Peers]);
         {p2p, logout, {User, Pid}} ->
             io:format("[client]: ~p logged out~n", [User]),
@@ -55,6 +64,10 @@ loop(Peers) ->
         {p2p, msg, Msg, {User, _Pid}} ->
             io:format("[client] ~p: ~p~n", [User, Msg]),
             loop(Peers);
+        {'EXIT', Pid, _Why} ->
+            {User, Pid} = lists:keyfind(Pid, 2, Peers),
+            io:format("[client] ~p logged out~n", [User]),
+            loop(lists:keydelete(Pid, 2, Peers));
         X ->
             io:format("[client] received ~p~n", [X]),
             loop(Peers)
